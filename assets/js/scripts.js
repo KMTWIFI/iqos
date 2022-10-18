efba7651e0ae67a163261f90fc780002f00aeccf
MohamedRamadan10 <mramadan@spark-sys.com>
2022-10-18 20:36:33 +0200

    format date

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,6 +9,7 @@ $(function () {
 	const birthDate = $(".birthdate.ar");
 	if (birthDate.length) {
 		birthDate.bootstrapBirthday({
+			minAge: 22,
 			widget: {
 				wrapper: {
 					tag: "div",
@@ -96,3 +97,81 @@ $(function () {
 		});
 	}
 });
+
+(function () {
+	jQuery.extend(jQuery.validator.messages, {
+		required: "هذه الخانة مطلوبه.",
+		remote: "الرجاء تصحيح هذا المجال.",
+		email: "رجاء قم بإدخال بريد الكتروني صحيح.",
+		url: "أدخل رابط صحيح من فضلك.",
+		date: "ارجوك ادخل تاريخ صحيح.",
+		dateISO: "الرجاء إدخال تاريخ صالح (ISO).",
+		number: "من فضلك أدخل رقما صالحا.",
+		digits: "الرجاء إدخال أرقام فقط.",
+		creditcard: "الرجاء إدخال رقم بطاقة ائتمان صالحة.",
+		equalTo: "من فظلك ادخل نفس القيمة مرة أخرى.",
+		accept: "الرجاء إدخال قيمة بامتداد صالح.",
+		maxlength: jQuery.validator.format("الرجاء إدخال ما لا يزيد عن {0} حرف."),
+		minlength: jQuery.validator.format("الرجاء إدخال {0} حرف على الأقل."),
+		rangelength: jQuery.validator.format(
+			"الرجاء إدخال قيمة يتراوح طولها بين {0} و {1} حرفًا."
+		),
+		letterMaxlength: jQuery.validator.format(
+			"الرجاء إدخال ما لا يزيد عن {0} حرف."
+		),
+		range: jQuery.validator.format("الرجاء إدخال قيمة بين {0} و {1}."),
+		max: jQuery.validator.format("الرجاء إدخال قيمة أقل من أو تساوي {0}."),
+		min: jQuery.validator.format("الرجاء إدخال قيمة أكبر من أو تساوي {0}."),
+	});
+
+	$("#form_id").validate({
+		rules: {
+			birthday: {
+				date: true,
+			},
+		},
+		errorPlacement: function (error, element) {
+			var invalidElement = $(element)
+				.parents(".form-group")
+				.find(".invalid-feedback");
+			invalidElement.each(function () {
+				$(this).html(error);
+				$(this).show();
+			});
+			// invalidElement.html(error);
+			// invalidElement.show();
+		},
+	});
+
+	("use strict");
+})();
+
+$("#form_id").on("submit", function (event) {
+	var birthDay = checkOnBirthDay(event);
+	console.log($(this).valid());
+	if ($(this).valid() && birthDay) {
+		return;
+	}
+	event.preventDefault();
+});
+
+$("#birthday").change(function (event) {
+	checkOnBirthDay(event);
+});
+
+function checkOnBirthDay(event) {
+	let birthday = $("[name=birthday]").val();
+	if ($("[name=birthday]").length > 0) {
+		if (birthday == null || birthday == "") {
+			$("select").css("border-color", "#dc3545");
+			$(".invalid-feedback").show();
+			return false;
+		} else {
+			$("select").css("border-color", "green");
+			$(".invalid-feedback").hide();
+
+			return true;
+		}
+	}
+	return true;
+}
