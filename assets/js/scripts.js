f298596712e2c6f94e8d31154c086048b67a56d4
MohamedRamadan10 <mramadan@spark-sys.com>
2022-10-20 06:06:56 +0200

    phone regex

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,156 +1,201 @@
 $(function () {
-   // const date = $("#date");
-   // if (date) {
-   //    date.datepicker({
-   //       dateFormat: "dd-mm-yy",
-   //       duration: "fast",
-   //    });
-   // }
-   const birthDate = $(".birthdate.ar");
-   if (birthDate.length) {
-      birthDate.bootstrapBirthday({
-         minAge: 21,
-         widget: {
-            wrapper: {
-               tag: "div",
-               class: "row",
-            },
-            wrapperYear: {
-               use: true,
-               tag: "span",
-               class: "col",
-            },
-            wrapperMonth: {
-               use: true,
-               tag: "span",
-               class: "col",
-            },
-            wrapperDay: {
-               use: true,
-               tag: "span",
-               class: "col",
-            },
-            selectYear: {
-               name: "birthday[year]",
-               class: "form-control input-sm",
-            },
-            selectMonth: {
-               name: "birthday[month]",
-               class: "form-control input-sm",
-            },
-            selectDay: {
-               name: "birthday[day]",
-               class: "form-control input-sm",
-            },
-         },
-         text: {
-            dateFormat: "littleEndian",
-            year: "سنة",
-            month: "شهر",
-            day: "يوم",
-            months: {
-               short: [
-                  "يناير",
-                  "فبراير",
-                  "مارس",
-                  "ابريل",
-                  "مايو",
-                  "يونيو",
-                  "يوليو",
-                  "اغسطس",
-                  "سبتمبر",
-                  "اكتوبر",
-                  "نوفمبر",
-                  "ديسمبر",
-               ],
-               long: [
-                  "يناير",
-                  "فبراير",
-                  "مارس",
-                  "ابريل",
-                  "مايو",
-                  "يونيو",
-                  "يوليو",
-                  "اغسطس",
-                  "سبتمبر",
-                  "اكتوبر",
-                  "نوفمبر",
-                  "ديسمبر",
-               ],
-            },
-         },
-      });
-   }
+	// const date = $("#date");
+	// if (date) {
+	//    date.datepicker({
+	//       dateFormat: "dd-mm-yy",
+	//       duration: "fast",
+	//    });
+	// }
+	const birthDate = $(".birthdate.ar");
+	if (birthDate.length) {
+		birthDate.bootstrapBirthday({
+			minAge: 21,
+			widget: {
+				wrapper: {
+					tag: "div",
+					class: "row",
+				},
+				wrapperYear: {
+					use: true,
+					tag: "span",
+					class: "col",
+				},
+				wrapperMonth: {
+					use: true,
+					tag: "span",
+					class: "col",
+				},
+				wrapperDay: {
+					use: true,
+					tag: "span",
+					class: "col",
+				},
+				selectYear: {
+					name: "birthday[year]",
+					class: "form-control input-sm",
+				},
+				selectMonth: {
+					name: "birthday[month]",
+					class: "form-control input-sm",
+				},
+				selectDay: {
+					name: "birthday[day]",
+					class: "form-control input-sm",
+				},
+			},
+			text: {
+				dateFormat: "littleEndian",
+				year: "سنة",
+				month: "شهر",
+				day: "يوم",
+				months: {
+					short: [
+						"يناير",
+						"فبراير",
+						"مارس",
+						"ابريل",
+						"مايو",
+						"يونيو",
+						"يوليو",
+						"اغسطس",
+						"سبتمبر",
+						"اكتوبر",
+						"نوفمبر",
+						"ديسمبر",
+					],
+					long: [
+						"يناير",
+						"فبراير",
+						"مارس",
+						"ابريل",
+						"مايو",
+						"يونيو",
+						"يوليو",
+						"اغسطس",
+						"سبتمبر",
+						"اكتوبر",
+						"نوفمبر",
+						"ديسمبر",
+					],
+				},
+			},
+		});
+	}
 
-   const customRadio = $(".custom__radio input[name='age']");
-   const form = $(".form__btn");
-   const btnLink = $(".btn__link");
-   if (customRadio) {
-      customRadio.on("change", function () {
-         if ($(this).val() === "up-21") {
-            form.fadeIn();
-            btnLink.fadeOut();
-         } else {
-            btnLink.fadeIn();
-            form.fadeOut();
-         }
-      });
-   }
+	const customRadio = $(".custom__radio input[name='age']");
+	const form = $(".form__btn");
+	const btnLink = $(".btn__link");
+	if (customRadio) {
+		customRadio.on("change", function () {
+			if ($(this).val() === "up-21") {
+				form.fadeIn();
+				btnLink.fadeOut();
+			} else {
+				btnLink.fadeIn();
+				form.fadeOut();
+			}
+		});
+	}
 });
 
 (function () {
-   $("#form_id").validate({
-      rules: {
-         birthday: {
-            date: true,
-         },
-         name: "required",
-         mobile: "required",
-         privacy: "required",
-      },
-      errorPlacement: function (error, element) {
-         var invalidElement = $(element)
-            .parents(".form-group")
-            .find(".invalid-feedback");
-         invalidElement.each(function () {
-            $(this).html(error);
-            $(this).show();
-         });
-      },
-   });
+	// $.validator.addMethod("customphone", function (value, element) {
+	// 	return this.optional(element) || /^01[0125][0-9]{8}$/.test(value);
+	// });
 
-   ("use strict");
+	$("#form_id").validate({
+		rules: {
+			birthday: {
+				date: true,
+			},
+			name: "required",
+			mobile: "required",
+			privacy: "required",
+		},
+		errorPlacement: function (error, element) {
+			var invalidElement = $(element)
+				.parents(".form-group")
+				.find(".invalid-feedback");
+			invalidElement.each(function () {
+				$(this).html(error);
+				$(this).show();
+			});
+		},
+	});
+
+	("use strict");
 })();
 
 $("#form_id").on("submit", function (event) {
-   var birthDay = checkOnBirthDay(event);
+	const birthDay = checkOnBirthDay(event);
+	const name = checkOnName(event);
+	const mobile = checkOnMobile(event);
 
-   if ($(this).valid() && birthDay) {
-      return;
-   }
-   event.preventDefault();
+	if ($(this).valid() && birthDay && name && mobile) {
+		return;
+	}
+	event.preventDefault();
 });
 
 $("#birthday").change(function (event) {
-   checkOnBirthDay(event);
+	checkOnBirthDay(event);
+});
+$("#name").change(function (event) {
+	checkOnName(event);
+});
+$("#mobile").change(function (event) {
+	checkOnMobile(event);
 });
 
 function checkOnBirthDay(event) {
-   let birthday = $("[name=birthday]").val();
-   let nameInput = $("[name=name]").val();
-   let mobileInput = $("[name=mobile]").val();
-   let privacyInput = $("[name=privacy]").val();
-   if ($("[name=birthday]").length > 0) {
-      if (birthday == null || birthday == "") {
-         $("select").css("border-color", "#dc3545");
-         $(".invalid-feedback").show();
-         return false;
-      } else {
-         $("select").css("border-color", "green");
-         $(".invalid-feedback").hide();
+	let birthday = $("[name=birthday]").val();
 
-         return true;
-      }
-   }
-   return true;
+	if ($("[name=birthday]").length > 0) {
+		if (birthday == null || birthday == "") {
+			$("select").css("border-color", "#dc3545");
+			$(".invalid-feedback").show();
+			return false;
+		} else {
+			$("select").css("border-color", "green");
+			$(".invalid-feedback").hide();
+
+			return true;
+		}
+	}
+	return true;
+}
+
+function checkOnName(event) {
+	let nameInput = $("[name=name]").val();
+	if ($("[name=name]").length > 0) {
+		if (nameInput.length === 0) {
+			$("[name=name]").css("border-color", "#dc3545");
+			$("[name=name]").next().show();
+			return false;
+		} else {
+			$("[name=name]").css("border-color", "green");
+			$("[name=name]").next().hide();
+
+			return true;
+		}
+	}
+	return true;
+}
+
+function checkOnMobile(event) {
+	let mobileInput = $("[name=mobile]").val();
+
+	if ($("[name=mobile]").length > 0) {
+		if (mobileInput.length === 0 || !mobileInput.match(/^01[0125][0-9]{8}$/)) {
+			$("[name=mobile]").css("border-color", "#dc3545");
+			$("[name=mobile]").next().show();
+			return false;
+		} else {
+			$("[name=mobile]").css("border-color", "green");
+			$("[name=mobile]").next().hide();
+
+			return true;
+		}
+	}
+	return true;
 }
